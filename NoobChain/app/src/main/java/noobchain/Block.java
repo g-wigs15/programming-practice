e3ec13c0f4dd57a76776ae7b236bdc745a401bbd
gwigs <kaganluvstacos@gmail.com>
2024-12-19 14:24:43 -0500

    Block Chain

---
+++ NoobChain/app/src/main/java/noobchain/Block.java
@@ -0,0 +1,43 @@
+package noobchain;
+import java.util.Date;
+
+public class Block {
+    
+    public String hash;
+	public String previousHash;
+	private String data; //our data will be a simple message.
+	private final long timeStamp; 
+    private int n; // was called nonce, but I dont like british terms (YUCK)
+
+    // Constructor for Block Class
+    public Block(String data,String previousHash ) {
+		
+        this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+        this.hash = calculateHash(); 
+	}
+
+    // Calls the Sha256 from StringUtil to create a hash for the Block Object in the main method.
+    public String calculateHash(){
+        
+        String calculatedhash = StringUtil.applySha256( 
+			
+            previousHash + Long.toString(timeStamp) + Integer.toString(n) + data 
+		);
+
+	return calculatedhash;
+    }
+
+    // 
+    public void mineBlock(int diff){
+        
+        String target = new String(new char[diff]).replace('\0', '0');//Create a string with difficulty * "0" 
+        
+        while(!hash.substring( 0, diff).equals(target)) {
+			n++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+    }
+}
---
+++ NoobChain/app/src/main/java/noobchain/StringUtil.java
@@ -0,0 +1,38 @@
+package noobchain;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class StringUtil {
+    
+    /*
+     * Don’t worry too much if you don’t understand the contents of this helper method, 
+     * all you need to know is that it takes a string and applies SHA256 algorithm to it, 
+     * and returns the generated signature as a string.
+     */
+
+    public static String applySha256(String input){		
+        
+        try {
+            
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+
+                if (hex.length() == 1)
+                    hexString.append('0');
+
+                hexString.append(hex);            
+            }
+            return hexString.toString();
+        } 
+        catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+           
+            throw new RuntimeException(e);
+        }
+    }
+}

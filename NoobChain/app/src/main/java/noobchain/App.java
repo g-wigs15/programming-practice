2205ae27d70bd221c8367243175d9eb372dc08c5
gwigs <kaganluvstacos@gmail.com>
2024-12-20 08:45:32 -0500

    update update

--- NoobChain/app/src/main/java/noobchain/App.java
+++ NoobChain/app/src/main/java/noobchain/App.java
@@ -9,7 +9,7 @@ import com.google.gson.GsonBuilder;
 public class App {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-    public static int difficulty = 5;
+    public static int difficulty = 5; // Should be changed to a larger number if we want this to be secure enough. Litecoin's dif: 442,592
 
     public static void main(String[] args) {
 		
